c8c6f006c762e39e498ee4e2616e744d66bf1892
woohyuckk <woohyuckk@gmail.com>
2025-02-04 20:09:48 +0900

    refactor: toast 기능 복구

--- src/store/myPokemonSlice.js
+++ src/store/myPokemonSlice.js
@@ -1,9 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 import pokemonList from'../utils/mokdata'
+import { Slide, toast } from "react-toastify";
 const initialState = {
   myPokemon: JSON.parse(localStorage.getItem("myPokemon")) || [],
 };
 
+const toastOption = {
+  position: "bottom-right",
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: false,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "light",
+  transition: Slide,
+};
 const myPokemonSlice = createSlice({
   name: "myPokemon",
   initialState,
@@ -14,22 +26,26 @@ const myPokemonSlice = createSlice({
       const newPokemon = pokemonList.find((pokemon) => pokemon.id === pokemonId);
       const MAX_POKEMON = 6;
 
+
+
       if (hasPokemon) {
-        alert("🦄 이미 선택된 포켓몬입니다.");
+        toast("🦄 이미 선택된 포켓몬입니다.", toastOption);
         return;
       }
       if (state.myPokemon.length === MAX_POKEMON) {
-        alert("❌ 포켓몬은 6마리까지 소유 가능합니다.");
+        toast("❌ 포켓몬은 6마리까지 소유 가능합니다", toastOption);
         return;
       }
 
       state.myPokemon.push(newPokemon);
+      toast(`✅ ${newPokemon.korean_name} 추가됨!`, toastOption);
       localStorage.setItem("myPokemon", JSON.stringify(state.myPokemon));
     },
 
     removeMyPokemon: (state, action) => {
       state.myPokemon = state.myPokemon.filter((pokemon) => pokemon.id !== action.payload);
       localStorage.setItem("myPokemon", JSON.stringify(state.myPokemon));
+      toast("✅ 포켓몬이 제거되었습니다.", toastOption);
     },
   },
 });
